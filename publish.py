94dd4c5ef0ec638d77dc03bd58372f1b32012ccc
DulhacNinja <cookieziislove@gmail.com>
2025-04-06 13:15:59 +0300

    oops

--- publish.py
+++ publish.py
@@ -1,24 +1,2 @@
-SOURCES = {
-    "Bucuresti": [1, 2, 3],
-    "Cluj-Napoca": [4, 5],
-    "Timisoara": [6],
-    "Iasi": [7, 8],
-    "Brasov": [9],
-    "Constanta": [10, 11],
-    "Craiova": [12],
-    "Galati": [13],
-    "Ploiesti": [14],
-    "Braila": [15],
-    "Oradea": [16, 17],
-    "Arad": [18],
-    "Pitesti": [19],
-    "Bacau": [20],
-    "Sibiu": [21],
-    "Miercurea Ciuc": [22],
-    "Deva": [23],
-    "Bistrita": [24],
-    "Ramnicu Valcea": [25],
-    "Suceava": [26]
-}
-PUBLICATIONS_COUNT = 10
-SUBSCRIPTIONS_COUNT = 5
+
+
